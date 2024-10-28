817dd116e23427a3a4cbb1b45110c6e495ec2145
vinicius <vinicius.messias4321@gmail.com>
2024-10-28 13:20:06 -0300

    modificando board

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ export default function Board(){
 
       <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
       <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
-      <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+      <Square value={squares[8]} onSquareClick={()=>handleClick(9)}/>
     </div>
 
     </>
